8d3508f9805445ccef28e9460cf7197692de0af5
Sam Baxter <sbaxter@cs.umass.edu>
2018-07-23 15:15:53 -0400

    Document Promise.prototype.then polyfill

--- src/promises.ts
+++ src/promises.ts
@@ -1,46 +1,75 @@
 import { Serialized, SerializableRuntime } from './runtime/serializable';
 import { Capture } from 'stopify-continuations/dist/src/runtime/runtime';
 
+const handlers = Symbol.for('handlers');
+
 export function polyfillPromises(rts: SerializableRuntime): void {
+
   const originalThen = Promise.prototype.then;
+  Promise.prototype.then =
+    function <T1, T2>(onFulfilled: (v: any) => T1 | PromiseLike<T1>,
+      onRejected: (e: any) => T2 | PromiseLike<T2>): Promise<T1 | T2> {
+      // Reify handlers queued on Promise object for serialization
+      this[handlers] = this[handlers] || [];
+      this[handlers].push({ onFulfilled, onRejected });
 
-  Promise.prototype.then = function <T1, T2>(resolve: (v: any) => T1 | PromiseLike<T1>,
-    reject: (e: any) => T2 | PromiseLike<T2>): Promise<T1 | T2> {
-    function wrapResolve(v: any) {
-      if (v instanceof Serialized) {
-        return v;
-      }
-
-      console.log('thenning');
-      return rts.rts.runtime(() => v, (doneV) => {
-        if (doneV instanceof Serialized) {
-          return;
+      const wrapFulfill = (v: any) => {
+        // A Promise chain has already been suspended. Early-exit chained
+        // callbacks.
+        if (v instanceof Serialized) {
+          return v;
         }
-        return resolve(doneV.value);
-      });
-    }
 
-    function wrapReject(e: any) {
-      if (e instanceof Capture) {
-        return rts.rts.runtime(() => { throw e; },
-          (v) => {
-            if (v.type === 'normal' && v.value instanceof Serialized) {
-              return v.value;
-            } else if (v.type === 'normal') {
-              return wrapResolve(v.value);
-            } else if (v.type === 'exception' && v.value instanceof Serialized) {
-              throw v.value;
-            } else {
-              return reject(v.value);
-            }
-          });
-      } if (reject) {
-        return reject(e);
-      } else {
-        throw e;
+        console.log('thenning', v);
+        // Wrap `onFulfilled` callback in runtime trampoline to handle
+        // suspending inside Promise callbacks.
+        return rts.rts.runtime(() => onFulfilled(v), (result) => {
+          // Release resolved handler
+          this[handlers].unshift();
+          return result.value;
+        });
       }
-    }
 
-    return originalThen.call(this, wrapResolve, wrapReject);
-  };
+      const wrapReject = (e: any) => {
+        // A Promise was rejected by initiating the capture of a continuation.
+        if (e instanceof Capture) {
+          // Rethrow the capture exception in a nested runtime to handle the
+          // exception.
+          return rts.rts.runtime(() => { throw e; },
+            (v) => {
+              if (v.type === 'normal' && v.value instanceof Serialized) {
+                // If we are serializing, just propogate the special return
+                // value.
+                return v.value;
+              } else if (v.type === 'normal') {
+                // If the resumption completes successfully, fulfill the
+                // promise.
+                const r = wrapFulfill(v.value);
+                this[handlers].unshift();
+                return r;
+              } else {
+                // If the resumption fails, reject the promise.
+                const r = onRejected(v.value);
+                this[handlers].unshift();
+                return r;
+              }
+            });
+        } else if (onRejected) {
+          // A user exception was thrown, so forward to the `onRejected`
+          // function if it exists.
+          const r = onRejected(e);
+          this[handlers].unshift();
+          return r;
+        } else {
+          // An unhandled user expception was thrown, propogate the throw.
+          throw e;
+        }
+      }
+
+      // Invoke the original `then` callback with wrapped handlers.
+      const thenned = originalThen.call(this, wrapFulfill, wrapReject);
+      // Reify the happens-before relation of the Promise chain.
+      this.resolvesTo = thenned;
+      return thenned;
+    };
 }
