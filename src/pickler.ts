8abd432ef81575fb12624a499a2afb37b19626f8
Sam Baxter <sbaxter@cs.umass.edu>
2018-07-24 15:52:05 -0400

    Prototype serializing Promises

--- src/pickler.ts
+++ src/pickler.ts
@@ -9,6 +9,7 @@ const native = '$__native';
 const user = '$__user';
 const index = '$__index';
 const functions = '$__functions';
+const promises = '$__promises';
 const symbols = '$__symbols';
 const globals = '$__globals';
 const cached = '$__cached';
@@ -21,11 +22,15 @@ const cache = Symbol.for('cache');
  */
 export class Pickler {
   /** Map Function objects to ids to support sharing */
-  private id_map: Map<Object, number> = new Map();
+  private fn_id_map: Map<Object, number> = new Map();
+  /** Map Promises to ids to support sharing */
+  private promise_id_map: Map<Object, number> = new Map();
   /** The next id to assign a shared object that gets restructured */
   private fn_id = 0;
+  private promise_id = 0;
   /** Reified fn objects that get assigned to object for serialization */
   private fns: any[] = [];
+  private promises: any[] = [];
 
   private restructureProperties(key: string, value: any): any {
     let properties: any = {};
@@ -42,8 +47,7 @@ export class Pickler {
     delete o[key];
 
     // Maintain identity of serialized functions
-    let ix = this.id_map.get(value)
-
+    let ix = this.fn_id_map.get(value)
     if (ix !== undefined) {
       o[key] = { type: index, value: ix };
       return;
@@ -57,9 +61,40 @@ export class Pickler {
     this.fns.push(record);
 
     o[key] = { type: index, value: this.fn_id };
-    this.id_map.set(value, this.fn_id++);
+    this.fn_id_map.set(value, this.fn_id++);
 
     record.properties = this.restructureProperties(key, value);
+  }
+
+  private restructurePromise(key: string, value: Promise<any>, o: any): any {
+    delete o[key];
+
+    let ix = this.promise_id_map.get(value);
+    if (ix !== undefined) {
+      o[key] = { type: promises, value: ix };
+      return;
+    }
+
+    const record = { type: promises, value };
+    this.promises.push(record)
+
+    o[key] = { type: promises, value: this.promise_id };
+    this.promise_id_map.set(value, this.promise_id++);
+  }
+
+  private restructureMap(o: Map<any, any>): any {
+    const keys = Array.from(o.keys());
+    for (const key of keys) {
+      const value = o.get(key);
+      o.delete(key);
+
+      const boxKey = { box: key };
+      this.restructure(boxKey);
+      const boxVal = { box: value };
+      this.restructure(boxVal);
+
+      o.set(key, value);
+    }
   }
 
   private restructure(o: any): any {
@@ -71,9 +106,8 @@ export class Pickler {
           return;
         }
 
-        if (o instanceof Promise) {
-          console.log('Promise');
-          return;
+        if (o instanceof Map) {
+          return this.restructureMap(o);
         }
 
         const props = Object.getOwnPropertyNames(o);
@@ -85,12 +119,15 @@ export class Pickler {
                 const cacheWrapper = { type: cached, value: value[cache] };
                 this.restructureFn('value', value[cache], cacheWrapper);
                 o[key] = cacheWrapper;
-                break;
+                continue;
               }
               this.restructureFn(key, value, o);
               break;
             case 'object':
-              if (value === global) {
+              if (value instanceof Promise) {
+                this.restructurePromise(key, value, o);
+                continue;
+              } else if (value === global) {
                 o[key] = { type: globals };
                 continue;
               }
@@ -108,6 +145,11 @@ export class Pickler {
     }
   }
 
+  serializePromise(map: Map<any, any>, p: Promise<any>): any {
+    const reified = map.get(p);
+    return reified;
+  }
+
   /**
    * Write the object `o` to a `Buffer`.
    *
@@ -118,16 +160,21 @@ export class Pickler {
     const box: any = { box: o };
     // Restructures Function objects and prototypes as serializable properties on `box`.
     this.restructure(box);
-    box[functions] = this.fns.map(foo => {
-      return {
-        type: foo.type,
-        value: foo.type === native ? {
-          type: symbols,
-          value: Symbol.keyFor(foo.value[nativeSym]),
-        } : foo.value.toString(),
-        properties: foo.properties,
-      };
-    });
+    box[functions] = this.fns.map(foo => ({
+      type: foo.type,
+      value: foo.type === native ? {
+        type: symbols,
+        value: Symbol.keyFor(foo.value[nativeSym]),
+      } : foo.value.toString(),
+      properties: foo.properties,
+    }));
+
+    box[promises] = this.promises.map(promise =>
+      ({
+        type: promise.type,
+        value: this.serializePromise(box.box.promises, promise.value)
+      }));
+    delete box.box.promises;
 
     try {
       // Leverage v8 serialization API to preserve sharing
@@ -150,6 +197,8 @@ export class Pickler {
 export class Depickler {
   /** Map Function objects to ids to support sharing */
   private fn_map: Map<number, Function> = new Map();
+  /** Map Promises to ids to support sharing */
+  private promise_map: Map<number, Promise<any>> = new Map();
 
   private filterFnProperties(obj: { [key: string]: any }): string[] {
     const properties = Object.getOwnPropertyNames(obj);
@@ -199,6 +248,8 @@ export class Depickler {
                 o[key] = this.fn_map.get(value.value);
               } else if (value && value.type === globals) {
                 o[key] = global;
+              } else if (value && value.type === promises) {
+                o[key] = this.promise_map.get(value.value);
               } else {
                 this.dispatch(value);
               }
@@ -209,6 +260,31 @@ export class Depickler {
         }
         break;
     }
+  }
+
+  private reconstructPromise(serial: any): Promise<any> {
+    if (serial.type !== promises) {
+      throw new Error(`Attempting to reconstruct a promise, but got ${serial}`);
+    }
+
+    const reified = serial.value;
+    if (typeof reified === 'number') {
+      return this.promise_map.get(reified)!;
+    }
+
+    switch (reified.status) {
+      case 'pending':
+        console.log('pending');
+        break;
+      case 'fulfilled':
+        return Promise.resolve(reified.value);
+      case 'rejected':
+        break;
+      default:
+        throw new Error(`unreachable: ${reified}`);
+    }
+
+    return Promise.resolve();
   }
 
   private reconstruct(o: any): any {
@@ -224,6 +300,19 @@ export class Depickler {
         this.reconstructProperties(serial, ix);
       } else {
         this.fn_map.set(ix, eval(serial.value.value))
+      }
+    });
+
+    o[promises].forEach((serial: any, ix: number) => {
+      const p = this.reconstructPromise(serial);
+      this.promise_map.set(ix, p);
+      this.promise_map.set(serial.value, p);
+    });
+    this.dispatch(o[promises].map((o: any) => o.value));
+    o[promises].map((o: any) => o.value).forEach((p: any, ix: number) => {
+      if (p.resolvesTo) {
+        const promise = this.promise_map.get(p)!;
+        promise.then(p.resolvesTo);
       }
     });
 
