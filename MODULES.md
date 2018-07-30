8d46fa95a37a2d7122b61f97495643195788a51d
Sam Baxter <sbaxter@cs.umass.edu>
2018-07-30 10:25:36 -0400

    More documentation for writing modules extending checkpoint behavior

--- MODULES.md
+++ MODULES.md
@@ -5,6 +5,29 @@ Checkpoints offer a foundation for building libraries which suspend execution
 while communicating with external services, and restore from the checkpoint
 upon their completion.
 
+## The Module Interface
+
+Modules make use of the `checkpoint` method of a `CheckpointRuntime`
+instance. This means modules must lazily "register" exported functions after
+a `CheckpointRuntime` object has been instantiated. The interface for
+defining a module should export a `register` function, which takes a
+`CheckpointRuntime` object as input, and returns an object containing the
+functions to be exported.
+
+```ts
+export function register(runtime: CheckpointRuntime) {
+  ...
+  return { `<exported functions>` };
+}
+```
+
+User code can consume modules by calling `register` on the `require`'d module.
+
+```js
+const runtime = require('./src/index');                 // `require` a `CheckpointRuntime` instance
+const mod = require('<module-path>').register(runtime); // Register an extension module
+```
+
 ## HTTP
 
 HTTP requests are the canonical example of asynchronous I/O. We develop a
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,29 +1,46 @@
 import { CheckpointRuntime } from '../runtime/checkpointable';
 const needle = require('needle');
 
+/**
+ * Register checkpointing functions which invoke HTTP requests and resume
+ * checkpoints with the response body.
+ */
 export function register(runtime: CheckpointRuntime) {
   function request(method: string, uri: string, options: any) {
+    // Return the result of the checkpoint. This captures and serializes the
+    // continuation.
     return runtime.checkpoint(k =>
+      // Once the continuation is captured and serialized, invoke an HTTP
+      // request.
       needle(method, uri, { ...options, json: true })
-        .then((response: any) => ({
+        .then((response: any) =>
+        // Return a conductor action result with the response body, and the
+        // serialized continuation as the `state`. The `echo` action forwards
+        // the HTTP response back to the main conductor action, which resumes
+        // from the continuation with the response.
+        ({
           action: '/whisk.system/utils/echo',
           params: response.body,
           state: { $continuation: k },
         })));
   }
 
+  // HTTP GET
   function get(uri: string, options: any) {
     return request('GET', uri, options);
   }
 
+  // HTTP PUT
   function put(uri: string, options: any) {
     return request('PUT', uri, options);
   }
 
+  // HTTP POST
   function post(uri: string, options: any) {
     return request('POST', uri, options);
   }
 
+  // HTTP DELETE
   function Delete(uri: string, options: any) {
     return request('DELETE', uri, options);
   }
